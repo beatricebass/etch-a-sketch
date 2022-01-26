8fa810a70d590c2a2789e1e7f41365d5c78cdb88
Beatrice <beatricebass94@gmail.com>
2022-01-25 21:58:04 -0500

    create grid, start hover function

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
-let mainGrid = document.querySelector('#mainGrid');
+// create the grid
 
-
+// get div where grid will be contained
+const mainGrid = document.querySelector(".mainGrid");
 rowNum = 16;
 
 function createGrid() {
-    for (i=0; i < rowNum; i++) {
-        let rowBox = document.createElement('div');
-        rowBox.setAttribute("id", "square");
-        mainGrid.appendChild(rowBox);
-        for (j=1; j < rowNum; j++) {
-          let box = document.createElement('div');
-       
-          box.setAttribute("id", "column");
-          rowBox.appendChild(box);
-        };
-      };
-    
+    for (i = 0; i < rowNum; i++) {
+        let row = document.createElement('div');
+        row.setAttribute("class", "row");
+        for (j = 0; j < rowNum; j++) {
+            let column = document.createElement("div");
+            column.setAttribute("class", "square");
+            row.appendChild(column);
+        }
+        mainGrid.appendChild(row);
+    };
 }
 
 createGrid();
+addEventListeners();
+
+function addColour() {
+    let squares = document.querySelectorAll(".square");
+    for (i = squares.length - 1; i >= 0; i--)
+    sqaures.item(s).setAttribute("class", "coloured");
+}
 
 
-let coloured = Array.from(document.querySelectorAll("#column"));
-coloured.forEach(color => color.addEventListener("mouseenter", color.classList.add("coloured")));
 
-console.log(coloured);
+function addEventListeners() {
+    let squares = document.querySelectorAll(".square");
+    console.log(squares);
+        for (x = 0; x < squares.length; x++) {
+            squares.item(x).addEventListener("mouseover", addColour);
+    }
+}
