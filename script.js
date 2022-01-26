6ba5ba9d1e8ec89221cbe80453f44ffc20e18fd0
Beatrice <beatricebass94@gmail.com>
2022-01-26 14:55:53 -0500

    hover function complete, reset button in progress

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// create the grid
 
 // get div where grid will be contained
 const mainGrid = document.querySelector(".mainGrid");
 rowNum = 16;
 
+// create grid
 function createGrid() {
     for (i = 0; i < rowNum; i++) {
         let row = document.createElement('div');
@@ -17,21 +17,68 @@ function createGrid() {
     };
 }
 
+let squareHandler = function(square){
+    square.addEventListener("mouseover", mouseoverHandler);
+
+    if (square.document.getAttribute("class") = "coloured") {
+        square.removeEventListener("")
+    }
+}
+
+
+let mouseoverHandler = (event) => {
+    // here, we don't know about square... but we have
+    // the event object. So event.currentTarget is
+    //  the same reference as square in the other function!
+    event.currentTarget.setAttribute("class", "coloured")
+    console.log(event)
+}
+  
+function addEventListeners() {
+    let squares = document.querySelectorAll(".square");
+  
+    squares.forEach( squareHandler );
+}
+
+function removeEventListeners() {
+    let squares = document.querySelectorAll(".square");
+  
+    squares.forEach( squareHandler );
+}
 createGrid();
 addEventListeners();
 
-function addColour() {
-    let squares = document.querySelectorAll(".square");
-    for (i = squares.length - 1; i >= 0; i--)
-    sqaures.item(s).setAttribute("class", "coloured");
+function resetGrid() {
+    removeEventListeners();
 }
 
+// // when mouse hovers over the square, its class is changed to .coloured and CSS implements a colour change
+// function addEventListeners() {
+//     let squares = document.querySelectorAll(".square");
+//         squares.forEach(square => {square.addEventListener("mouseover", () => {
+//                 square.setAttribute("class", "coloured");
+//             });
+//     })
+// }
 
 
-function addEventListeners() {
-    let squares = document.querySelectorAll(".square");
-    console.log(squares);
-        for (x = 0; x < squares.length; x++) {
-            squares.item(x).addEventListener("mouseover", addColour);
-    }
-}
+
+
+// // in order to have the `forEach` *see* the handler, we
+// // need to define it. 
+// const mouseoverHandler = (event)=>{
+//     event.currentTarget.setAttribute("class", "colored")
+//   }
+  
+//   function addEventListeners() {
+//     let squares = document.querySelectorAll(".square");
+  
+//     // we can inline this function, if we like. And
+//     //  either a ()=>{} style function or a traditional
+//     //  one will work.
+//     squares.forEach(function(square){
+//        square.addEventListener("mouseover", mouseoverHandler);
+//     });
+//   }
+
+
